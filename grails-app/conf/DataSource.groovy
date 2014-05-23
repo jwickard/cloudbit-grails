4083aa52fa9b3ce4782da9db13e1c6c3c6d526d3
Joel Wickard <jwickard@gmail.com>
2014-05-23 16:02:46 -0500

    Fix nested datasource declaration

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -29,17 +29,15 @@ environments {
     }
     production {
         dataSource {
-            dataSource {
-                dbCreate = "update"
-                driverClassName = "org.postgresql.Driver"
-                dialect = org.hibernate.dialect.PostgreSQLDialect
+            dbCreate = "update"
+            driverClassName = "org.postgresql.Driver"
+            dialect = org.hibernate.dialect.PostgreSQLDialect
 
-                uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+            uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
 
-                url = "jdbc:postgresql://"+uri.host+uri.path
-                username = uri.userInfo.split(":")[0]
-                password = uri.userInfo.split(":")[1]
-            }
+            url = "jdbc:postgresql://"+uri.host+uri.path
+            username = uri.userInfo.split(":")[0]
+            password = uri.userInfo.split(":")[1]
         }
     }
 }
